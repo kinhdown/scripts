1897dc1201773b8a263499c81cb1846809af71c7
scripts <zygjex@gmail.com>
2021-06-07 10:00:03 +0800

    Update

--- jd_zoo.js
+++ jd_zoo.js
@@ -35,13 +35,9 @@ const pKHelpAuthorFlag = true;//是否助力作者PK  true 助力，false 不助
 let cookiesArr = [];
 $.cookie = '';
 $.inviteList = [];
-$.pkInviteList = [
-  "sSKNX-MpqKOJsNu8mJ7RA9BJMup4tAAmPcPPPhBUWYKUJ19UKeC8EAoKeUXELi4u",
-];
+$.pkInviteList = [];
 $.secretpInfo = {};
-$.innerPkInviteList = [
-
-];
+$.innerPkInviteList = [];
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
     cookiesArr.push(jdCookieNode[item])
@@ -95,6 +91,12 @@ if ($.isNode()) {
   if (new Date().getHours()>= 9) {
     res = await getAuthorShareCode() || [];
     res2 = await getAuthorShareCode('http://cdn.trueorfalse.top/e528ffae31d5407aac83b8c37a4c86bc/') || [];
+  }
+  if (new Date().getHours() === 9 || (new Date().getHours() === 10 && new Date().getMinutes() < 11)) {
+    $.innerPkInviteList.push('sSKNX-MpqKPS7Le4m5rbBpODDLhoZ9ruJViTqJpv4c2Lm2-TfJwzRBS82zBEzkXK');
+    $.innerPkInviteList.push('sSKNX-MpqKOJsNvSzMSZfAM9H7GwE_7GAGP6h5-yWMFC6rsV_bSQHlBmw28Arg');
+    $.innerPkInviteList.push('sSKNX-MpqKOJsNvM-u2vdyHH5x3rrYdCxJ-sE_oy8C69xCinRiEdQ3bib4w42g');
+    $.innerPkInviteList.push('sSKNX-MpqKOJsNu-ms-IVdCm3z528hK5o2UJgQWu0nCM-DsOo6H6pwAnkox1F2YO');
   }
   if (pKHelpAuthorFlag) {
     $.innerPkInviteList = getRandomArrayElements([...$.innerPkInviteList, ...res, ...res2, ...res3], [...$.innerPkInviteList, ...res, ...res2, ...res3].length);
